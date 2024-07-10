17900401a462834f07f75e375cc36fbe8a8072f4
Oliver Wahlen <oliver.wahlen@infinit-group.de>
2024-07-10 09:34:12 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dynamo Service
+Demonstrator project building a Spring Boot application with DynamoDB.
+The project is based on [https://www.baeldung.com/spring-data-dynamodb](https://www.baeldung.com/spring-data-dynamodb).
---
+++ src/main/kotlin/com/wahlen/dynamoservice/DynamoserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.wahlen.dynamoservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DynamoserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<DynamoserviceApplication>(*args)
+}
